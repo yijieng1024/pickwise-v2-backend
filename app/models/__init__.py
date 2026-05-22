3e5c4437c9791499b2a83350559bb3246b63af92
yijieng1024 <yijieng1024@gmail.com>
2026-05-22 18:01:10 +0800

    fix: remove comment

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,2 @@
-# app/models/__init__.py
-
-# We import the models here so that SQLModel's metadata registry 
-# knows they exist before Alembic tries to generate migrations.
-
 from .user import User
 from .laptop import Laptop, LaptopBase, LaptopCreate, LaptopRead, LaptopEmbedding
